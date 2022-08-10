fcf469dfd3431bc2fb958540c5f4b8c09d2f3a4b
Andrea055 <vivanco11.ac@gmail.com>
2022-08-10 19:25:37 +0200

    update script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Multisim 12 on Linux
+
+This script will run Multisim 12 on Linux through Wine and Wineget
+
+Currently only Multisim 12 is supported but I'm working on 14.3 version, see issue for details.
+
+![img]("/screen.png")
--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-WINEARCH=win32 WINEPREFIX=~/winetest/multisim12/ winetricks mdac27 jet40 dotnet40 mfc80 vcrun2005
-winetricks winxp
+WINEARCH=win32 WINEPREFIX=~/wol/multisim12/ winetricks mdac27 jet40 dotnet40 mfc80 vcrun2005
+WINEARCH=win32 WINEPREFIX=~/wol/multisim12/ winetricks winxp
+wget https://github.com/Windows-On-Linux/Multisim-On-Linux/releases/download/12/Multisim12.zip 2>&1 | tee -a wget_log
+mkdir Multisim12Installer
+unzip Multisim12.zip -d Multisim12Installer
+cd Multisim12Installer
+WINEARCH=win32 WINEPREFIX=~/wol/multisim12/ wine setup.exe
+echo "Installer is terminated, enjoy!"
+
